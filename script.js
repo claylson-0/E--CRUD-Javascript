d165f27cc4b00bc9f14d450cb5a4bf57a5586896
claylson-0 <64693150+Claylson-0@users.noreply.github.com>
2021-10-19 16:20:17 -0300

    Utiliza Json

--- script.js
+++ script.js
@@ -1,54 +1,38 @@
-var produtos=[
-    {
-        nome:"Molho de tomate",
-        marca:"Quero",
-        preco:1.30,
-        estoque:"Sim"
-    },
-    {
-        nome:"Pão de forma",
-        marca:"Panco",
-        preco:7,
-        estoque:"Não"
-    },
-    {
-        nome:"Detergente",
-        marca:"Ype",
-        preco:2,
-        estoque:"Sim"
-    }
-];
+var jsonPromise = fetch('./JSON.json').then(results => results.json())
 
-function criaProduto(){
+
+function criaProduto() {
+    jsonPromise.then(criaProdutoJson)
+}
+
+function criaProdutoJson(produtos) {
     var nomeProduto = document.getElementById("nome-produto").value;
     var marca = document.getElementById("marca").value;
     var preco = document.getElementById("preco").value;
     var estoque = document.getElementById("estoque").value;
 
-    var objProduto={
-        nome:nomeProduto,
-        marca:marca,
-        preco:preco,
-        estoque:estoque
-    }
-    produtos.push(objProduto);
+    produtos.push({
+        nome: nomeProduto,
+        marca: marca,
+        preco: preco,
+        estoque: estoque
+    })
 
+    limpaCampo()
+    listaProduto();
+}
+
+function limpaCampo() {
     document.getElementById("nome-produto").value = "";
     document.getElementById("marca").value = "";
     document.getElementById("preco").value = "";
     document.getElementById("estoque").value = "";
-
-    console.log(objProduto);
-    console.log(produtos);
-    console.log(produtos[0].nome);
-    listaProduto();
-
 }
 
-function listaProduto(){
+function listaProdutoJson(produtos) {
     var tbLista = document.getElementById("tabela-lista-produto");
-    var elemento="";
-    for(var i = 0;i<produtos.length;i++){
+    var elemento = "";
+    for (var i = 0; i < produtos.length; i++) {
         elemento += `<tr>
         <td>${produtos[i].nome}</td>
         <td>${produtos[i].marca}</td>
@@ -62,39 +46,49 @@ function listaProduto(){
         </td>
         <tr>`;
     }
-    tbLista.innerHTML=elemento;
+    tbLista.innerHTML = elemento;
 }
 
+function listaProduto() {
+    jsonPromise.then(listaProdutoJson)
+}
 listaProduto();
 
-function deletaProduto(i){
-    produtos.splice(i,1);
+function deletaProduto(i) {
+    jsonPromise.then(produtos =>
+        produtos.splice(i, 1)
+    )
     listaProduto()
 }
 
-function editaProduto(i){
-    document.getElementById("nome-produto").value=produtos[i].nome
-    document.getElementById("marca").value=produtos[i].marca
-    document.getElementById("preco").value=produtos[i].preco
-    document.getElementById("estoque").value=produtos[i].estoque
+function editaProduto(i) {
+    jsonPromise.then(function (produtos) {
+        document.getElementById("nome-produto").value = produtos[i].nome
+        document.getElementById("marca").value = produtos[i].marca
+        document.getElementById("preco").value = produtos[i].preco
+        document.getElementById("estoque").value = produtos[i].estoque
 
-    document.getElementById("btn-cria").innerHTML="Editar"
-    document.getElementById("btn-cria").setAttribute("onclick",`reescreveProduto(${i})`)
+        document.getElementById("btn-cria").innerHTML = "Editar"
+        document.getElementById("btn-cria").setAttribute("onclick", `reescreveProduto(${i})`)
+    })
+
 }
 
-function reescreveProduto(i){
-    produtos[i].nome=document.getElementById("nome-produto").value
-    produtos[i].marca=document.getElementById("marca").value
-    produtos[i].preco=document.getElementById("preco").value
-    produtos[i].estoque=document.getElementById("estoque").value
+function reescreveProduto(i) {
+    jsonPromise.then(function (produtos) {
+        var nomeProduto = document.getElementById("nome-produto").value
+        var marca = document.getElementById("marca").value
+        var preco = document.getElementById("preco").value
+        var estoque = document.getElementById("estoque").value
+        produtos[i].nome = nomeProduto
+        produtos[i].marca = marca
+        produtos[i].preco = preco
+        produtos[i].estoque = estoque
+        
+        document.getElementById("btn-cria").innerHTML = "Criar"
+        document.getElementById("btn-cria").setAttribute("onclick", "criaProduto()")
 
-    document.getElementById("nome-produto").value = "";
-    document.getElementById("marca").value = "";
-    document.getElementById("preco").value = "";
-    document.getElementById("estoque").value = "";
-
-    document.getElementById("btn-cria").innerHTML="Criar"
-    document.getElementById("btn-cria").setAttribute("onclick","criaProduto()")
-
-    listaProduto()
+        limpaCampo()
+        listaProduto()
+    })
 }
